7ebe6806477a2f99e9694a37d35820052be08df2
VarSriv <varnika.srivastava@gmail.com>
2018-05-23 10:28:30 +0530

    NLP Project Team 1
    by Varnika(336),varun kumar(338) , varuni (343)

---
+++ constants.py
@@ -0,0 +1,16 @@
+"""
+Contains constants related to the dataset.
+"""
+
+minimum_scores = [-1, 2, 1, 0, 0, 0, 0, 0, 0]
+maximum_scores = [-1, 12, 6, 3, 3, 4, 4, 30, 60]
+
+# configurable
+#DATASET_DIR = '../Data/AES/' # Path to the dataset
+DATASET_DIR = 'C:/Users/vrnsr/Desktop/NLP_Proj'
+#GLOVE_DIR = '../Glove/' # Path to the GloVe files
+GLOVE_DIR = 'C:/Users/vrnsr/Downloads/glove.6B'
+TRAINED_MODELS_DIR = '../Trained-Models/' # Path to where models are to be saved.
+
+# non configurable
+SAVE_DIR = './'
